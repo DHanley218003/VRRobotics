937680bcde595b2e8660617ddddf6d51ca2b3926
clementlb <clemeleberre@hotmail.fr>
2019-05-16 10:49:56 +0100

    Initial addition of unity project

---
+++ ArmRobot/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateObject : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject ObjectToRotate;
+
+    public void RotateMyObject()
+    {
+        float sliderValue = GetComponent<Slider>().value;
+        ObjectToRotate.transform.Rotate(sliderValue * speed * Time.deltaTime, 0, 90);
+    }
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
