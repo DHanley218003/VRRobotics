9c9c71f8809c7baf7927e44c8a980fa61cc26fd3
clementlb <clemeleberre@hotmail.fr>
2019-05-20 12:13:06 +0100

    Controlling Arduino from Unity
    I have a problem with the rotation of object but I can control the robot with unity interface. The robot don't have the same shape on unity and IRL

---
+++ Controling Arduino from Unity/Assets/ControlArduino.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+using System.Threading;
+
+public class ControlArduino : MonoBehaviour
+{
+    SerialPort serial;
+    public string myString;
+    public float comRapidity = 2.0f;
+    public string portName;
+    float temps = 0.0f;
+    float delay = 0.0f;
+    bool setPort = true;
+
+    public int servoDegre1; //Degré value
+    public Transform servo1;
+    public Slider sliderServo1;
+    string A;
+    public int servoDegre2; //Degré value
+    public Transform servo2;
+    public Slider sliderServo2;
+    string B;
+    public int servoDegre3; //Degré value
+    public Transform servo3;
+    public Slider sliderServo3;
+    string C;
+    public int servoDegre4; //Degré value
+    public Transform servo4;
+    public Slider sliderServo4;
+    string D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serial = new SerialPort();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (servoDegre1 != (sliderServo1.value))
+        {
+            if (servoDegre1 < (sliderServo1.value))
+            {
+                servoDegre1 = servoDegre1 + 1;
+            }
+            if (servoDegre1 > (sliderServo1.value))
+            {
+                servoDegre1 = servoDegre1 - 1;
+            }
+        }
+        servo1.localRotation = Quaternion.AngleAxis(-servoDegre1, Vector3.back);
+        A = servoDegre1.ToString("000");
+        if (servoDegre2 != (sliderServo2.value))
+        {
+            if (servoDegre2 < (sliderServo2.value))
+            {
+                servoDegre2 = servoDegre2 + 1;
+            }
+
+            if (servoDegre2 > (sliderServo2.value))
+            {
+                servoDegre2 = servoDegre2 - 1;
+            }
+        }
+        servo2.localRotation = Quaternion.AngleAxis(-servoDegre2, Vector3.up);
+        B = servoDegre2.ToString("000");
+        if (servoDegre3 != (sliderServo3.value))
+        {
+            if (servoDegre3 < (sliderServo3.value))
+            {
+                servoDegre3 = servoDegre3 + 1;
+            }
+
+            if (servoDegre3 > (sliderServo3.value))
+            {
+                servoDegre3 = servoDegre3 - 1;
+            }
+        }
+        servo3.localRotation = Quaternion.AngleAxis(-servoDegre3, Vector3.up);
+        C = servoDegre3.ToString("000");
+        if (servoDegre4 != (sliderServo4.value))
+        {
+            if (servoDegre4 < (sliderServo4.value))
+            {
+                servoDegre4 = servoDegre4 + 1;
+            }
+
+            if (servoDegre4 > (sliderServo4.value))
+            {
+                servoDegre4 = servoDegre4 - 1;
+            }
+        }
+        servo4.localRotation = Quaternion.AngleAxis(-servoDegre4, Vector3.up);
+        D = servoDegre4.ToString("000");
+
+        myString = string.Concat(A, B, C, D);
+
+        temps = Time.time;
+        if ((delay + comRapidity) < temps)
+        {
+            Envoyer();
+            delay = temps;
+            //Debug.Log (myString);
+        }
+
+    }
+    public void Envoyer()
+    {
+        if (setPort == true)
+        {
+            serial.PortName = portName;
+            serial.Parity = Parity.None;
+            serial.BaudRate = 9600;
+            serial.DataBits = 8;
+            serial.StopBits = StopBits.One;
+            serial.Open();
+            setPort = false;
+        }
+        serial.Write(myString + "\n");
+
+    }
+}
---
+++ Controling Arduino from Unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+using System.Threading;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    //communication
+    SerialPort serial;
+    public string myString;
+    float temps = 0.0f;
+    float delay = 0.0f;
+    public float comRapidity = 2.0f;
+    public string portName;
+    bool setPort = true;
+
+    //Servo 1 
+    public int servoDegre1; //Degré value
+    public Transform servo1;
+    public Slider sliderServo1;
+    public Button buttonS1plus;
+    public Button buttonS1moins;
+    public Text textS1;
+    string A;
+
+    // Servo 2
+    public int servoDegre2; //Degré value
+    public Transform servo2;
+    public Slider sliderServo2;
+    public Button buttonS2plus;
+    public Button buttonS2moins;
+    public Text textS2;
+    string B;
+
+    // Autre servo test
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //communication
+        serial = new SerialPort();
+
+        //Button S1
+        buttonS1plus.onClick.AddListener(TaskOnClickS1plus);
+        buttonS1moins.onClick.AddListener(TaskOnClickS1moins);
+
+        //Button S2
+        buttonS2plus.onClick.AddListener(TaskOnClickS2plus);
+        buttonS2moins.onClick.AddListener(TaskOnClickS2moins);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Servo 1 
+        if (servoDegre1 != (sliderServo1.value))
+        {
+            if (servoDegre1 < (sliderServo1.value))
+            {
+                servoDegre1 = servoDegre1 + 1;
+            }
+            if (servoDegre1 > (sliderServo1.value))
+            {
+                servoDegre1 = servoDegre1 - 1;
+            }
+        }
+        servo1.localRotation = Quaternion.AngleAxis(-servoDegre1, Vector3.up);
+        textS1.text = servoDegre1.ToString() + "°";
+
+        //Servo 2 
+        if (servoDegre2 != (sliderServo2.value))
+        {
+            if (servoDegre2 < (sliderServo2.value))
+            {
+                servoDegre2 = servoDegre2 + 1;
+            }
+
+            if (servoDegre2 > (sliderServo2.value))
+            {
+                servoDegre2 = servoDegre2 - 1;
+            }
+        }
+        servo2.localRotation = Quaternion.AngleAxis(-servoDegre2, Vector3.up);
+        textS2.text = servoDegre2.ToString() + "°";
+
+        //communication
+
+        A = servoDegre1.ToString("000");
+        B = servoDegre2.ToString("000");
+
+        myString = string.Concat(A, B);
+
+        temps = Time.time;
+        if ((delay + comRapidity) < temps)
+        {
+            Envoyer();
+            delay = temps;
+            //Debug.Log (myString);
+        }
+
+    }
+
+    // button S1
+    void TaskOnClickS1plus()
+    {
+        (sliderServo1.value) = (sliderServo1.value) + 1;
+    }
+
+    void TaskOnClickS1moins()
+    {
+        (sliderServo1.value) = (sliderServo1.value) - 1;
+    }
+    // button S2
+    void TaskOnClickS2plus()
+    {
+        (sliderServo2.value) = (sliderServo2.value) + 1;
+    }
+
+    void TaskOnClickS2moins()
+    {
+        (sliderServo2.value) = (sliderServo2.value) - 1;
+    }
+
+    //communication
+    public void Envoyer()
+    {
+        if (setPort == true)
+        {
+            serial.PortName = portName;
+            serial.Parity = Parity.None;
+            serial.BaudRate = 9600;
+            serial.DataBits = 8;
+            serial.StopBits = StopBits.One;
+            serial.Open();
+            setPort = false;
+        }
+        serial.Write(myString + "\n");
+
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Application ending after " + Time.time + "seconds");
+        serial.Close();
+    }
+
+}
